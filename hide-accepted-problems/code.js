425644078a2115f68c4414f6acd3199cd72b4fd8
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2020-05-16 00:51:07 +0530

    Update code.js

--- hide-accepted-problems/code.js
+++ hide-accepted-problems/code.js
@@ -10,8 +10,29 @@
 
 (function() {
     'use strict';
-    var x = document.getElementsByClassName("accepted-problem");
-    var i;
-    for(let ele of x)
-        ele.style.display = 'none';
+    var acceptedHidden = false, button, accepted, myButton;
+    const toggleProblemStatus = () => {
+        console.log('hi');
+        acceptedHidden = !acceptedHidden;
+        if(acceptedHidden){
+            for(let problemRow of accepted)
+                problemRow.style.display = 'none';
+            myButton.innerHTML = 'Show Accepted';
+        }else{
+            for(let problemRow of accepted)
+                problemRow.style.display = '';
+            myButton.innerHTML = 'Hide accepted';
+        }
+    }
+    button = document.createElement('button');
+    button.style.float = 'right';
+    button.id = 'accepted-toggle-button';
+    button.onclick = toggleProblemStatus;
+    button.innerHTML = 'Hide accepted';
+    console.log(button);
+    document.getElementsByClassName('second-level-menu')[0].appendChild(button);
+    accepted = document.getElementsByClassName("accepted-problem");
+    console.log(accepted);
+    myButton = document.getElementById('accepted-toggle-button');
+    console.log('hi', 'myButton : ', myButton);
 })();
